e9ed34bb4943d9251c2a69cd9deb8bfb955ec0e2
Lubosz Sarnecki <lubosz@gmail.com>
2012-12-13 17:13:07 +0100

    filesystem: add classes and ignore build files
    add generated classes from VDX
    update gitignore

---
+++ src/SequenceOrganizer.cpp
@@ -0,0 +1,26 @@
+/*
+    Copyright © 2012 The KoRE Project
+
+    This file is part of KoRE.
+
+    KoRE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    KoRE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with KoRE.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "SequenceOrganizer.h"
+
+namespace KoRE {
+  SequenceOrganizer::SequenceOrganizer() {
+  }
+}
+
