db23530b378c1c3812dfb97e18b7fa4b8c299582
Lubosz Sarnecki <lubosz@gmail.com>
2013-01-24 12:28:47 +0100

    fix camelcase

--- src/KoRE/DataTypes.cpp
+++ src/KoRE/DataTypes.cpp
@@ -16,7 +16,7 @@
   You should have received a copy of the GNU General Public License
   along with KoRE.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include "core/datatypes.h"
+#include "KoRE/DataTypes.h"
 
 unsigned int kore::DatatypeUtil::getSizeFromGLdatatype(GLenum datatype) {
     switch (datatype) {
