bb98508f696d1007019fc55bfbc0a4c967b99643
Dominik Lazarek <dominik.lazarek@gmail.com>
2013-03-22 19:14:24 +0100

    Implemented material property loading

--- src/KoRE/Loader/SceneLoader.cpp
+++ src/KoRE/Loader/SceneLoader.cpp
@@ -184,9 +184,10 @@ void kore::SceneLoader::loadSceneGraph(const aiNode* ainode,
 
       // Load the material for this mesh. Note that for every mesh, there is
       // a material in Assimp.
-     // Material* materialComponent = new Material;
-
-      
+      Material* materialComponent = new Material;
+      loadMaterialProperties(materialComponent,
+                             aiscene->mMaterials[aimesh->mMaterialIndex]);
+      node->addComponent(materialComponent);
 
     // Make additional copies for any more meshes
     for (uint iMesh = 1; iMesh < ainode->mNumMeshes; ++iMesh) {
@@ -199,11 +200,20 @@ void kore::SceneLoader::loadSceneGraph(const aiNode* ainode,
       const aiMesh* aimesh = aiscene->mMeshes[ainode->mMeshes[iMesh]];
       std::string meshName = MeshLoader::getInstance()
         ->getMeshName(aimesh, ainode->mMeshes[iMesh]);
+
       Mesh* mesh = ResourceManager::getInstance()
         ->getMesh(szScenePath, meshName);
+
       MeshComponent* meshComponent = new MeshComponent;
       meshComponent->setMesh(mesh);
       copyNode->addComponent(meshComponent);
+
+      // Load the material for this mesh. Note that for every mesh, there is
+      // a material in Assimp.
+      Material* materialComponent = new Material;
+      loadMaterialProperties(materialComponent,
+        aiscene->mMaterials[aimesh->mMaterialIndex]);
+      node->addComponent(materialComponent);
     }
   }
 
@@ -254,35 +264,101 @@ std::string kore::SceneLoader::getLightName(const aiLight* pAiLight,
 
 void kore::SceneLoader::
   loadMaterialProperties(Material* koreMat, const aiMaterial* aiMat) {
-    for (uint i = 0; i < aiMat->mNumProperties; ++i) {
-      const aiMaterialProperty* aiMatProp = aiMat->mProperties[i];
-      if (aiMatProp->mSemantic != aiTextureType_NONE) {
-        continue;  // skip texture properties
-      }
+    // Note(dlazarek): Because of the somewhat ugly Assimp-api concerning
+    // material-properties, currently the safest way is to process all material
+    // constants one by one. Keep in mind that this list hast to be extended
+    // if the assimp material-API changes.
+    
+    // Temp-vars to retrieve the values from assimp
+    aiColor3D colValue;
+    int intValue;
+    float floatValue;
 
-      std::string name(aiMatProp->mKey.C_Str());
-      GLuint glDataType;
-      
-      switch (aiMatProp->mType) {
-      case aiPropertyTypeInfo::aiPTI_Float:
-        {
-          
-        } break;
-      default:
-        break;
-      }
-        
-      //koreMat->addValue(name,                        )
+    // Diffuse color/reflectivity
+    if (aiMat->Get(AI_MATKEY_COLOR_DIFFUSE, colValue) == AI_SUCCESS) {
+      glm::vec3* pValue = new glm::vec3(colValue.r, colValue.g, colValue.b);
+      koreMat->addValue("Diffuse Color", GL_FLOAT_VEC3, pValue);
+    }
 
+    //Specular
+    if (aiMat->Get(AI_MATKEY_COLOR_SPECULAR, colValue) == AI_SUCCESS) {
+      glm::vec3* pValue = new glm::vec3(colValue.r, colValue.g, colValue.b);
+      koreMat->addValue("Specular Color", GL_FLOAT_VEC3, pValue);
+    }
+
+    // Ambient
+    if (aiMat->Get(AI_MATKEY_COLOR_AMBIENT, colValue) == AI_SUCCESS) {
+      glm::vec3* pValue = new glm::vec3(colValue.r, colValue.g, colValue.b);
+      koreMat->addValue("Ambient Color", GL_FLOAT_VEC3, pValue);
+    }
+
+    // Emissive
+    if (aiMat->Get(AI_MATKEY_COLOR_EMISSIVE, colValue) == AI_SUCCESS) {
+      glm::vec3* pValue = new glm::vec3(colValue.r, colValue.g, colValue.b);
+      koreMat->addValue("Emissive Color", GL_FLOAT_VEC3, pValue);
+    }
+
+    // Transparent color
+    if (aiMat->Get(AI_MATKEY_COLOR_TRANSPARENT, colValue) == AI_SUCCESS) {
+      glm::vec3* pValue = new glm::vec3(colValue.r, colValue.g, colValue.b);
+      koreMat->addValue("Transparent Color", GL_FLOAT_VEC3, pValue);
+    }
+
+    // Reflective color
+    if (aiMat->Get(AI_MATKEY_COLOR_REFLECTIVE, colValue) == AI_SUCCESS) {
+      glm::vec3* pValue = new glm::vec3(colValue.r, colValue.g, colValue.b);
+      koreMat->addValue("Reflective Color", GL_FLOAT_VEC3, pValue);
+    }
+
+    // Enable Wireframe
+    if (aiMat->Get(AI_MATKEY_ENABLE_WIREFRAME, intValue) == AI_SUCCESS) {
+      int* pValue = new int(intValue);
+      koreMat->addValue("Enable Wireframe", GL_INT, pValue);
+    }
+
+    // Twosided
+    if (aiMat->Get(AI_MATKEY_TWOSIDED, intValue) == AI_SUCCESS) {
+      int* pValue = new int(intValue);
+      koreMat->addValue("Twosided", GL_INT, pValue);
+    }
+
+    // Opacity
+    if (aiMat->Get(AI_MATKEY_OPACITY, floatValue) == AI_SUCCESS) {
+      float* pValue = new float(floatValue);
+      koreMat->addValue("Opacity", GL_FLOAT, pValue);
+    }
+
+    // Shininess
+    if (aiMat->Get(AI_MATKEY_SHININESS, floatValue) == AI_SUCCESS) {
+      float* pValue = new float(floatValue);
+      koreMat->addValue("Shininess", GL_FLOAT, pValue);
+    }
+
+    // Shininess-Strength
+    if (aiMat->Get(AI_MATKEY_SHININESS_STRENGTH, floatValue) == AI_SUCCESS) {
+      float* pValue = new float(floatValue);
+      koreMat->addValue("Shininess-Strength", GL_FLOAT, pValue);
+    }
+
+    // Refraction index
+    if (aiMat->Get(AI_MATKEY_REFRACTI, floatValue) == AI_SUCCESS) {
+      float* pValue = new float(floatValue);
+      koreMat->addValue("Refraction index", GL_FLOAT, pValue);
+    }
+
+    // Bump-scaling
+    if (aiMat->Get(AI_MATKEY_BUMPSCALING, floatValue) == AI_SUCCESS) {
+      float* pValue = new float(floatValue);
+      koreMat->addValue("Bump strength", GL_FLOAT, pValue);
     }
 }
 
 void kore::SceneLoader::loadMaterialTextures(TexturesComponent* texComponent,
-                                             const aiMaterial* aiMat ) {
+                                             const aiMaterial* aiMat) {
     for (uint i = 0; i < aiMat->mNumProperties; ++i) {
       const aiMaterialProperty* aiMatProp = aiMat->mProperties[i];
       if (aiMatProp->mSemantic == aiTextureType_NONE) {
         continue;  // skip non-texture properties
-      } 
+      }
     }
 }
