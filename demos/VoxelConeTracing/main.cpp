845c93e1a4ab7a4d95c777961d1642cde78ab25b
Dominik Lazarek <dominik.lazarek@gmail.com>
2013-03-29 16:55:59 +0100

    Initial commit: Copied mainglfw-demo over to new VoxelConeTracing project.

---
+++ demos/VoxelConeTracing/main.cpp
@@ -0,0 +1,153 @@
+/*
+ Copyright (c) 2012 The KoRE Project
+
+  This file is part of KoRE.
+
+  KoRE is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  KoRE is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with KoRE.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <GL/glew.h>
+#include <GL/glfw.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <ctime>
+#include <vector>
+
+#include "KoRE/GLerror.h"
+#include "KoRE/ShaderProgram.h"
+#include "KoRE/Components/MeshComponent.h"
+#include "KoRE/Components/TexturesComponent.h"
+#include "KoRE/Operations/RenderMesh.h"
+#include "KoRE/Operations/BindOperations/BindAttribute.h"
+#include "KoRE/Operations/BindOperations/BindUniform.h"
+#include "KoRE/Operations/BindOperations/BindTexture.h"
+#include "KoRE/Operations/UseFBO.h"
+#include "KoRE/Operations/UseShaderProgram.h"
+#include "KoRE/ResourceManager.h"
+#include "KoRE/RenderManager.h"
+#include "KoRE/Components/Camera.h"
+#include "KoRE/SceneNode.h"
+#include "KoRE/Timer.h"
+#include "KoRE/Texture.h"
+#include "KoRE/FrameBuffer.h"
+#include "Kore/Passes/FrameBufferStage.h"
+#include "Kore/Passes/ShaderProgramPass.h"
+#include "KoRE/Passes/NodePass.h"
+#include "KoRE/Events.h"
+
+int main(void) {
+  int running = GL_TRUE;
+
+  // Initialize GLFW
+  if (!glfwInit()) {
+    kore::Log::getInstance()->write("[ERROR] could not load window manager\n");
+    exit(EXIT_FAILURE);
+  }
+
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 4);
+  glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 2);
+  glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwOpenWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+  // Open an OpenGL window
+  if (!glfwOpenWindow(800, 600, 8, 8, 8, 8, 24, 8, GLFW_WINDOW)) {
+    kore::Log::getInstance()->write("[ERROR] could not open render window\n");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  // disable v-sync
+  glfwSwapInterval(0);
+
+  // initialize GLEW
+  glewExperimental = GL_TRUE;
+  if (glewInit()) {
+    kore::Log::getInstance()->write("[ERROR] could not open initialize extension manager\n");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  // Init gl-states
+  // glEnable(GL_VERTEX_ARRAY);
+    
+  // log versions
+  int GLFWmajor, GLFWminor, GLFWrev;
+  glfwGetVersion(&GLFWmajor, &GLFWminor, &GLFWrev);
+  kore::Log::getInstance()
+    ->write("Render Device: %s\n",
+            reinterpret_cast<const char*>(
+            glGetString(GL_RENDERER)));
+  kore::Log::getInstance()
+    ->write("Vendor: %s\n",
+            reinterpret_cast<const char*>(
+            glGetString(GL_VENDOR)));
+  kore::Log::getInstance()
+    ->write("OpenGL version: %s\n",
+            reinterpret_cast<const char*>(
+            glGetString(GL_VERSION)));
+  kore::Log::getInstance()
+    ->write("GLSL version: %s\n",
+             reinterpret_cast<const char*>(
+             glGetString(GL_SHADING_LANGUAGE_VERSION)));
+  kore::Log::getInstance()
+    ->write("GLFW version %i.%i.%i\n",
+             GLFWmajor, GLFWminor, GLFWrev);
+  kore::Log::getInstance()
+    ->write("GLEW version: %s\n",
+            reinterpret_cast<const char*>(
+            glewGetString(GLEW_VERSION)));
+
+  // enable culling and depthtest
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
+  
+  glClearColor(1.0f,1.0f,1.0f,1.0f);
+
+  kore::Timer the_timer;
+  the_timer.start();
+  double time = 0;
+  float cameraMoveSpeed = 4.0f;
+  
+  int oldMouseX = 0;
+  int oldMouseY = 0;
+  glfwGetMousePos(&oldMouseX,&oldMouseY);
+    
+  // Main loop
+  while (running) {
+    time = the_timer.timeSinceLastCall();
+    kore::SceneManager::getInstance()->update();
+
+    kore::GLerror::gl_ErrorCheckStart();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT |GL_STENCIL_BUFFER_BIT);
+    kore::RenderManager::getInstance()->renderFrame();
+    glfwSwapBuffers();
+    kore::GLerror::gl_ErrorCheckFinish("Main Loop");
+
+    // Check if ESC key was pressed or window was closed
+    running = !glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED);
+  }
+
+  // Close window and terminate GLFW
+  glfwTerminate();
+
+  // Exit program
+  exit(EXIT_SUCCESS);
+};
